38c214a68971216f6f3a4257b046e9c6332a2237
Robert Casey <rtcasey12@gmail.com>
2018-07-25 14:44:27 -0500

    Still debugging 6_largest_number, iter #3 or 4 in progress.

--- data_struct_algo/week3/6_largest_number.cpp
+++ data_struct_algo/week3/6_largest_number.cpp
@@ -1,3 +1,7 @@
+// Robert T. Casey
+// Coursera DSA - week 3
+// 6_largest_number.cpp
+
 #include <algorithm>
 #include <sstream>
 #include <iostream>
@@ -8,13 +12,44 @@ using std::vector;
 using std::string;
 
 string largest_number(vector<string> a) {
-  //write your code here
-  std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
-    ret << a[i];
-  }
+  
+	// Basic algorithm:  turn each string into an integer
+	// so you have a vector of integers
+	// sort that
+	// dump it back into a string....in reverse (since sort does in ascending order)
+	// done
+
+	std::string::size_type sz;
+	std::vector< int > numbers;
+	int num_elems = a.size();
+	//std::cout << "Adding numbers to int array:" << std::endl;
+	for (int sdx = 0; sdx < num_elems; ++sdx)
+	{
+		int new_num = std::stoi(a.at(sdx), &sz);
+		//std::cout << new_num << " ";
+		numbers.push_back(new_num);
+	}
+	//std::cout << std::endl;
+	
+	std::sort(numbers.begin(), numbers.end());
+	//std::cout << "Sorted numbers:" << std::endl;
+	/*for (int n = 0; n < numbers.size(); ++n)
+	{
+		std::cout << numbers.at(n) << " ";
+	}*/
+	//std::cout << std::endl;
+
+  std::stringstream ss_num;
   string result;
-  ret >> result;
+  for (std::vector< int >::reverse_iterator rit = numbers.rbegin();
+	  rit != numbers.rend(); 
+	  ++rit )
+  {
+	  ss_num << *rit;
+	  result += ss_num.str();
+	  ss_num.str("");  // reset
+  }
+	
   return result;
 }
 
