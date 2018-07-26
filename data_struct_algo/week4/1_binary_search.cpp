1399e0d0bd0607f6114b788e540faf3e486844d2
Robert Casey <rtcasey12@gmail.com>
2018-07-26 16:22:45 -0700

    Work in progress on binary search.

--- data_struct_algo/week4/1_binary_search.cpp
+++ data_struct_algo/week4/1_binary_search.cpp
@@ -1,12 +1,44 @@
+// Robert T. Casey
+// Coursera DSA - week 4
+// 1_binary_search.cpp
+
 #include <iostream>
 #include <cassert>
 #include <vector>
 
 using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
+int binary_search( const vector<int> & a,
+				   int left, 
+	               int right,
+	               int x) 
+{
+	// there needs to be  a check for left <= right
+	if (left <= right)
+	{
+		int mid = left + (right - 1) / 2;
+		/*std::cout << "left = " << left;
+		std::cout << ", mid = " << mid;
+		std::cout << ", right = " << right << std::endl;
+*/
+		if (a.at(mid) == x)  // Base case
+		{
+			return mid;
+		}
+		else // Recursive cases
+		{
+			if (x < a.at(mid))
+			{
+				return binary_search(a, left, mid - 1, x); // left
+			}
+			else
+			{
+				return binary_search(a, mid + 1, right, x); // right
+			}
+		}
+	}  // end if left <= right
+	// Failure case: value does not exist in array
+	return -1;
 }
 
 int linear_search(const vector<int> &a, int x) {
@@ -31,6 +63,7 @@ int main() {
   }
   for (int i = 0; i < m; ++i) {
     //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
+	std::cout << binary_search(a, 0, a.size() - 1, b[i]) << ' ';
+    //std::cout << linear_search(a, b[i]) << ' ';
   }
 }
