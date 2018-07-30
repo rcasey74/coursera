20285ae199190de72e16085f3be5dfef502f7c3c
Robert Casey <rtcasey12@gmail.com>
2018-07-29 17:15:08 -0700

    First locally working solution to number of inversions problem.

--- data_struct_algo/week4/4_num_inversions.cpp
+++ data_struct_algo/week4/4_num_inversions.cpp
@@ -1,15 +1,78 @@
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using std::vector;
 
-long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
-  long long number_of_inversions = 0;
-  if (right <= left + 1) return number_of_inversions;
-  size_t ave = left + (right - left) / 2;
-  number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
+int merge(vector<int> & arr, int low, int mid, int high)
+{
+	int i, j, k, num_inv = 0;
+	int n1 = mid - low + 1;
+	int n2 = high - mid;
+
+	std::unique_ptr< int[] > L(new int[n1]());
+	std::unique_ptr< int[] > R(new int[n2]());
+
+	for (i = 0; i < n1; ++i)
+	{
+		L[i] = arr[low + i];
+	}
+	for (j = 0; j < n2; ++j)
+	{
+		R[j] = arr[mid + j + 1];
+	}
+	i = 0, j = 0, k = low;
+	while (i < n1 && j < n2)
+	{
+		if (L[i] < R[j])
+		{
+			arr[k] = L[i];
+			++i;
+		}
+		else
+		{
+			arr[k] = R[j];
+			++j;
+			++num_inv;
+		}
+		++k;
+	}
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		++i;
+		++k;
+	}
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		++j;
+		++k;
+	}
+	return num_inv;
+}
+
+int mergesort(vector<int> & arr, int low, int high)
+{
+	int num_inv = 0;
+	if (low < high)
+	{
+		int mid = low + (high - low) / 2;
+		mergesort(arr, low, mid);
+		mergesort(arr, mid + 1, high);
+		num_inv = merge(arr, low, mid, high);
+	}
+	return num_inv;
+}
+
+long long get_number_of_inversions(vector<int> &a) {
+	//std::cout << "inside get_number_of_inversions" << std::endl;
+  long long number_of_inversions = mergesort( a, 0, a.size() - 1 );
+  /*for (int i = 0; i < a.size(); ++i)
+  {
+	  std::cout << a[i] << ", ";
+  }
+  std::cout << std::endl;*/
   return number_of_inversions;
 }
 
@@ -20,6 +83,5 @@ int main() {
   for (size_t i = 0; i < a.size(); i++) {
     std::cin >> a[i];
   }
-  vector<int> b(a.size());
-  std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+   std::cout << get_number_of_inversions(a) << '\n';
 }
