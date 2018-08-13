520046e35d6f4f50b1b66db53ef3cd4c1bc771f6
Robert Casey <rtcasey12@gmail.com>
2018-08-12 18:38:01 -0700

    Finally implemented a dp approach to coin change problem

--- data_struct_algo/week5/1b_change_dp.cpp
+++ data_struct_algo/week5/1b_change_dp.cpp
@@ -1,63 +1,43 @@
 // Robert T. Casey
 // Coursera DSA - week 5
-// 1_change_dp.cpp
+// 1b_change_dp.cpp
 // Problem spec:  Using dynamic programming, find the minimum number of coins to change the input value
 // into coins with denominations 1, 3, and 4
-// More generally, for a given integer k, we are trying to
-// minimize the sum ( a + b + c ),
-// where
-// a * x + b * y + c * z = k,
-// and
-// x = 1, y = 3, and z = 4.
-// DP: use prior results, plus some intelligent backtracking, etc.
-// constraints:  1 <= k <= 1000
-// Create integer array of size max_k (1000)
-// Carefully fill in the elements left to right
-// Return element at index k
 
 #include <iostream>
 #include <climits>
+#include <vector>
 
 int get_change(int n) {
-
-	int * C = new int[n];
-	C[0] = 0;
-	int * S = new int[n];
-	int k = 3;
-	int d[] = { 0,1,3,4 };
 	
-	int min = INT_MAX;
-	int coin;
-	for (int p = 1; p <= n; ++p)
+	std::vector< int > C( n + 1, 0 );
+	C[0] = 0;
+	const int k = 3; // number of coin denominations:  1, 3, 4
+	int d[k+1] = { 0, 1, 3, 4 };
+	// i = 1, 3, 4
+	// C[j] = INT_MAX, if j < 0
+	//        0,       if j = 0
+	//    min{  1 + C[ j - 4 ] 
+	//          1 + C[ j - 3 ] 
+	//          1 + C[ j - 1 ]
+	for (int j = 1; j <= n; ++j)
 	{
+		C[j] = 2000000000;
 		for (int i = 1; i <= k; ++i)
 		{
-			if (1 + C[p - d[i]] < min)
+			if (j >= d[i] && (1 + C[j - d[i]] < C[j]))
 			{
-				min = 1 + C[p - d[i]];
-				coin = i;
+				C[j] = 1 + C[j - d[i]];
 			}
-		}
-		C[p] = min;
-		S[p] = coin;
-	}
+		}	// end for i
+	}	// end for j
 
-	std::cout << "C[n] = " << std::endl;
-	for (int t = 1; t <= n; ++t)
+	/*for (int p = 0; p < C.size(); ++p)
 	{
-		std::cout << t << " : " << C[t] << " , ";
-		if (t % 20) std::cout << std::endl;
-	}
-
-	std::cout << std::endl;
-	std::cout << "S[n] = " << S[n] << std::endl;
-
+		std::cout << C[p] << " ";
+	}*/
 	return C[n];
-
-	delete[] C;
-	delete[] S;
-	
-}
+}	// end get_change
 
 int main() {
   int n;
