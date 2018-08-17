834ea5366e96dce55134027e33eed7556699978e
Robert Casey <rtcasey12@gmail.com>
2018-08-16 20:31:09 -0700

    Bug fix: off-by-one error with initial check.

--- data_struct_algo/week5/4_lcs2b.cpp
+++ data_struct_algo/week5/4_lcs2b.cpp
@@ -16,24 +16,6 @@ using std::vector;
 
 int lcs2(vector<int> &a, vector<int> &b ) {
 	
-	// Base case of recursion:  we're one past the end of one of the arrays
-	// and so no more elements available to handle
-	if (adx == a.size() || bdx == b.size() )
-	{
-		return 0;
-	}
-	if (a.at(adx) == b.at(bdx)) // compare current indices for each array
-	{
-		// Last elements line up: just move forward in both arrays
-		return 1 + lcs2(a, b, adx + 1, bdx + 1);
-	}
-	else
-	{
-		// Last elements mismatch: try moving forward one step in just one of the arrays
-		// And compare the 2 options, i.e. moving forward in a versus fwd in b.
-		return std::max(lcs2(a, b, adx, bdx + 1), lcs2(a, b, adx + 1, bdx));
-	}
-
 	int a_size = a.size(),
 		b_size = b.size();
 	
@@ -44,11 +26,11 @@ int lcs2(vector<int> &a, vector<int> &b ) {
 	}
 
 	// Init:  all the 0 indices will have 0 value, since the "length" of the common subsequence has to be 0.
-	for (int adx = 0; adx < a_size; ++adx)
+	for (int adx = 0; adx <= a_size; ++adx)
 	{
 		LCS[adx][0] = 0;
 	}
-	for (int bdx = 0; bdx < b_size; ++bdx)
+	for (int bdx = 0; bdx <= b_size; ++bdx)
 	{
 		LCS[0][bdx] = 0;
 	}
@@ -57,7 +39,7 @@ int lcs2(vector<int> &a, vector<int> &b ) {
 	{
 		for (int bdx = 1; bdx <= b_size; ++bdx)
 		{
-			if (a.at(adx) == b.at(bdx) )  // match, just add one to the previous LCS
+			if (a.at(adx-1) == b.at(bdx-1) )  // match, just add one to the previous LCS
 			{
 				LCS[adx][bdx] = 1 + LCS[adx - 1][bdx - 1];
 			}
