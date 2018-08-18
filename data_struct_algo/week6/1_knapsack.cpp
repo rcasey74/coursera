49a666408c8d6b5c68400d55bd69d9ffacc458cb
Robert Casey <rtcasey12@gmail.com>
2018-08-18 14:20:01 -0700

    New branch:  week6, initial commit of starter files.

---
+++ data_struct_algo/week6/1_knapsack.cpp
@@ -0,0 +1,38 @@
+﻿// Robert T. Casey
+// Coursera DSA - week 6
+// 1_knapsack.cpp
+// Problem spec:  Discrete knapsack, without repetitions.
+// You are given a set of bars of gold and your goal is to take as much gold as possible into
+// your bag. There is just one copy of each bar and for each bar you can either take it or not.
+// Task:  Given 𝑛 gold bars, find the maximum weight of gold that fits into a bag of capacity 𝑊.
+// Input Format:  The first line of the input contains the capacity 𝑊 of a knapsack and the number 𝑛 of bars
+// of gold.The next line contains 𝑛 integers 𝑤0, 𝑤1, . . ., 𝑤𝑛−1 defining the weights of the bars of gold.
+// Constraints:   1 ≤ 𝑊 ≤ 10^4; 1 ≤ 𝑛 ≤ 300; 0 ≤ 𝑤0, . . ., 𝑤𝑛−1 ≤ 10^5.
+// Output Format:  Output the maximum weight of gold that fits into a knapsack of capacity 𝑊.
+
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int optimal_weight(int W, const vector<int> &w) {
+  //write your code here
+  int current_weight = 0;
+  for (size_t i = 0; i < w.size(); ++i) {
+    if (current_weight + w[i] <= W) {
+      current_weight += w[i];
+    }
+  }
+  return current_weight;
+}
+
+int main() {
+  int n, W;
+  std::cin >> W >> n;
+  vector<int> w(n);
+  for (int i = 0; i < n; i++) {
+    std::cin >> w[i];
+  }
+  std::cout << optimal_weight(W, w) << '\n';
+}
---
+++ data_struct_algo/week6/3_placing_parentheses.cpp
@@ -0,0 +1,46 @@
+﻿// Robert T. Casey
+// Coursera DSA - week 6
+// 3_placing_parentheses.cpp
+// Problem spec:  In this problem, your goal is to add parentheses to a given arithmetic
+// expression to maximize its value.
+// Task: Find the maximum value of an arithmetic expression by specifying the order of applying its arithmetic
+// operations using additional parentheses.
+// Input Format: The only line of the input contains a string 𝑠 of length 2𝑛 + 1 for some 𝑛, with symbols
+// 𝑠0, 𝑠1, . . ., 𝑠2𝑛.Each symbol at an even position of 𝑠 is a digit(that is, an integer from 0 to 9) while
+// each symbol at an odd position is one of three operations from{ +,-,*}.
+// Constraints: 1 ≤ 𝑛 ≤ 14 (hence the string contains at most 29 symbols).
+// Output Format: Output the maximum possible value of the given arithmetic expression among different
+// orders of applying arithmetic operations.
+
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+using std::max;
+using std::min;
+
+long long eval(long long a, long long b, char op) {
+  if (op == '*') {
+    return a * b;
+  } else if (op == '+') {
+    return a + b;
+  } else if (op == '-') {
+    return a - b;
+  } else {
+    assert(0);
+  }
+}
+
+long long get_maximum_value(const string &exp) {
+  //write your code here
+  return 0;
+}
+
+int main() {
+  string s;
+  std::cin >> s;
+  std::cout << get_maximum_value(s) << '\n';
+}
