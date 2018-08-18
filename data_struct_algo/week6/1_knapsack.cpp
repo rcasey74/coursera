12740de2370df2fb3fd8e850a6ab27d960d05482
Robert Casey <rtcasey12@gmail.com>
2018-08-18 15:25:12 -0700

    First locally working (just 1 test) of 1_knapsack.cpp, discrete knapsack with no repetitions.

--- data_struct_algo/week6/1_knapsack.cpp
+++ data_struct_algo/week6/1_knapsack.cpp
@@ -10,21 +10,82 @@
 // Constraints:   1 ≤ 𝑊 ≤ 10^4; 1 ≤ 𝑛 ≤ 300; 0 ≤ 𝑤0, . . ., 𝑤𝑛−1 ≤ 10^5.
 // Output Format:  Output the maximum weight of gold that fits into a knapsack of capacity 𝑊.
 
-
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
+bool DecreaseOrderCompare(int i, int j)
+{
+	return i > j;	
+}
+
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
-    }
+
+  // Seems like a wise solution will involve sorting the bars first.
+  //std::sort(w.begin(), w.end(), DecreaseOrderCompare);
+  // List is now in decreasing order
+
+  // Just in case:  erase those elements > W, since we can never add those to the knapsack
+  // meh:  the input is const...cannot delete the elements
+  //while ( !w.empty() && *(w.begin()) > W )
+  //{
+	 // w.erase( w.begin() );
+  //}
+  int n = static_cast< int >(w.size());
+
+  // Set up a value matrix, which iterates
+  // over rows from i = 1 to n (basically the number of items), and
+  // over cols from w = 1 to W, where w is the size of the current knapsack under consideration
+
+  int ** val_mat = new int *[n+1];
+  for (int r = 0; r < n + 1; ++r)
+  {
+	  val_mat[r] = new int[W+1];
   }
-  return current_weight;
+  
+  // Init the first row and column to zero
+  for (int r = 0; r < n + 1; ++r)
+  {
+	  val_mat[r][0] = 0;
+  }
+  for (int c = 0; c < W + 1; ++c)
+  {
+	  val_mat[0][c] = 0;
+  }
+
+  //-------------------------------
+  // actual code that does stuff
+  int t_val = 0;
+  for (int i = 1; i <= n; ++i) // current number of items
+  {
+	  for (int curr_w = 1; curr_w <= W; ++curr_w)  // current trial weight of knapsack
+	  {
+		  val_mat[i][curr_w] = val_mat[i - 1][curr_w];
+		  if (w.at(i-1) <= curr_w)
+		  {
+			  t_val = val_mat[i - 1][curr_w - w.at(i-1)] + w.at(i-1);
+			  if (t_val > val_mat[i][curr_w])
+			  {
+				  val_mat[i][curr_w] = t_val;
+			  }
+		  }
+	  }	// end for w
+  }	// end for i
+  
+  //-------------------------------
+  // done - grab the max value
+  int max_val = val_mat[n][W];
+
+  // free resources
+  for (int d = 0; d < n + 1; ++d)
+  {
+	  delete[] val_mat[d];
+  }
+  delete[] val_mat;
+
+  return max_val;
 }
 
 int main() {
--- data_struct_algo/week6/3_placing_parentheses.cpp
+++ data_struct_algo/week6/3_placing_parentheses.cpp
@@ -6,7 +6,7 @@
 // Task: Find the maximum value of an arithmetic expression by specifying the order of applying its arithmetic
 // operations using additional parentheses.
 // Input Format: The only line of the input contains a string 𝑠 of length 2𝑛 + 1 for some 𝑛, with symbols
-// 𝑠0, 𝑠1, . . ., 𝑠2𝑛.Each symbol at an even position of 𝑠 is a digit(that is, an integer from 0 to 9) while
+// 𝑠0, 𝑠1, . . ., 𝑠2𝑛. Each symbol at an even position of 𝑠 is a digit(that is, an integer from 0 to 9) while
 // each symbol at an odd position is one of three operations from{ +,-,*}.
 // Constraints: 1 ≤ 𝑛 ≤ 14 (hence the string contains at most 29 symbols).
 // Output Format: Output the maximum possible value of the given arithmetic expression among different
